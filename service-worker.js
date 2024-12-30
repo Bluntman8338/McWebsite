ba86a54f33f0c5a5365beabe59fabb9bc0435d59
Bluntman8338 <mymediaemail8338@gmail.com>
2024-12-30 12:51:42 -0800

    updated to PWA

---
+++ service-worker.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = "mcserver-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/css/style.css",
+  "/js/script.js",
+  "/path-to-icon/icon-192x192.png",
+  "/path-to-icon/icon-512x512.png"
+];
+
+// Install the service worker and cache files
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Fetch resources from cache or network
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Update the service worker and clear old cache
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
